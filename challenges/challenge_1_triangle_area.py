735fd997b0f7bdf8e0c832669b30879bef6e0622
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-07-30 14:11:47 +0000

    Add challenge 1 solution

--- challenges/challenge_1_triangle_area.py
+++ challenges/challenge_1_triangle_area.py
@@ -1,12 +1,14 @@
 """
 Calculate the area of a triangle using Heron's formula
 """
+import math
+
 a = 3
 b = 4
 c = 5
 
 # Todo: replace with calculations for s and area
-s = None  # superperimeter
-area = None
+s = (a + b + c) / 2  # superperimeter
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-print(area)
+print(round(area, 2))
