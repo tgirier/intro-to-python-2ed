3f745b040b2a90bf453170b7af58dc784c4509a5
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-07-30 14:01:04 +0000

    Add problem 1 solution

--- problems/problem_1_area_calculator.py
+++ problems/problem_1_area_calculator.py
@@ -1,10 +1,9 @@
 """
 Calculate the area of a right-angle triangle
 """
-a = 3  # side 1
-b = 4  # side 2
+a = 3.1  # side 1
+b = 4.2  # side 2
 
-# Todo: calculate the area
-area = None
+area = a * b / 2
 
 print(area)
