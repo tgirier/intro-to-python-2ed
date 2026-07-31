20712068d67ad10e8c2d3a746bd770b4386360e8
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-07-31 14:36:25 +0000

    Add challenge 3 solution

--- challenges/challenge_3_time_target.py
+++ challenges/challenge_3_time_target.py
@@ -8,7 +8,26 @@ Time target game
 # Hit ENTER to stop
 # 2.789s
 # You were 0.211s off
+from random import randint
+from time import time
+
+def time_target(t):
+    input(f"{t}s test. Hit ENTER to start")
+    start = time()
+    input("Hit ENTER to stop")
+    end = time()
+
+    duration = end - start
+    diff = abs(t - duration)
+
+    print(f"{duration:.3}s")
+    print(f"You were {diff:.3} off")
+
 
 # Todo: get random number of seconds
+target = randint(2, 5)
 
 # Todo: run game 3 times
+time_target(target)
+time_target(target)
+time_target(target)
