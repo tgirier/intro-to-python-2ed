b6ec0eb1d9f2073b9ad4f188467398a38037d9dd
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-08-03 12:26:54 +0000

    Add problem 10 solution

--- problems/problem_10_card_hand.py
+++ problems/problem_10_card_hand.py
@@ -2,11 +2,22 @@
 Deal a hand of 5 cards
 """
 
+import random
+
 suits = ['♠︎', '♣︎', '♥︎', '♦︎']
-ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
+ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']        
 
 cards = []
 hand = []
 
+for suit in suits:
+    for rank in ranks:
+        cards.append(rank + suit)
 
+for _ in range(5):
+    card = random.choice(cards)
+    cards.remove(card)
+    hand.append(card)
+
+print(cards)
 print(hand)
