3d1d7fe0b7ce41b6668c5603fa8b44b2b208f415
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-08-03 10:24:56 +0000

    Add problem 9 solution

---
+++ examples/TGI/example_13_for_loops.py
@@ -0,0 +1,4 @@
+primes = [2, 3, 5, 7, 11, 13]
+
+for prime in sorted(primes):
+    print(f"{prime} squared is {prime ** 2}")
--- problems/problem_9_vowel_counter.py
+++ problems/problem_9_vowel_counter.py
@@ -4,4 +4,10 @@ Print how many vowels that an input word contains.
 
 word = input("Type a word: ")
 
-print('There are {} vowels in "{}"')
+vowels_count = 0
+
+for letter in word.lower():
+    if letter in 'aeiouy':
+        vowels_count +=1
+
+print(f'There are {vowels_count} vowels in "{word}"')
