be8f0f3e1f605bfa089047ea7b640ecbe0c99b39
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-08-03 13:16:58 +0000

    Add challenge 5 solution

--- challenges/challenge_5_quiz.py
+++ challenges/challenge_5_quiz.py
@@ -16,12 +16,35 @@ category = 'Country that starts with V'
 
 guessed = []
 
+# Create a loop with 4 try
+while True:
+    num_left = len(answers)
+    if num_left == 0:
+        print("Great Job!")
+        break
+    print(f"{num_left} left")
+
 # 4 left
-guess = input(f"Enter a {category.lower()} (q to quit): ")
+    guess = input(f"Enter a {category.lower()} (q to quit): ").title()
+
+    if guess.lower() == "q":
+        missed = ', '.join(answers)
+        print("You missed: " + missed)
+        break
+    elif guess in guessed:
+        print('Already guessed')
+    elif guess in answers:
+        guessed.append(guess)
+        answers.remove(guess)
+        print('Correct')
+    else:
+        print('Try again')
+
 
 # Correct!
 # Already guessed
 # Try again
+
 
 # Great job!
 # You missed: a, b, c, d
