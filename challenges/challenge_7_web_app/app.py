70c59783f5e6528645887c22b9577dd2195b6606
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-08-04 13:56:39 +0000

    Update challenge 7 solution

--- challenges/challenge_7_web_app/app.py
+++ challenges/challenge_7_web_app/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    redirect, 
+    render_template, 
+    request, 
+    session,
+    url_for,
+)
 
 app = Flask(__name__)
+app.secret_key = b'aetjlkf1146+-454:/6jdsljfe'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        session['name'] = request.form['name']
+    return render_template("index.html", 
+                           name=session.get('name', ''),
+                           num_clicks=session.get('num_clicks', 0))
+
+@app.route("/reset")
+def reset():
+    session['name'] = ''
+    return redirect(url_for('index'))
+
+@app.route("/click")
+def click():
+    session['num_clicks'] = session.get('num_clicks', 0) + 1
+    return {'num_clicks': session['num_clicks']}
 
 if __name__ == '__main__':
     app.run(debug=True)
