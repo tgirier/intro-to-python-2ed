db59d1b1da73547ec508ecd88e33c06a277c2954
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2026-08-04 13:29:42 +0000

    Update challenge 7 solution

--- challenges/challenge_7_web_app/app.py
+++ challenges/challenge_7_web_app/app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/about")
-def about():
-    return "<p>Hi, I'm a Flask application!</p>"
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
